3733fb00d960d0491e17d51eb63225eaa38b89af
Carl <cj@example.com>
2023-10-09 11:55:26 +0800

    (gallery finished)

--- view/login-page.php
+++ view/login-page.php
@@ -48,35 +48,6 @@ session_start();
     </form>
 </div>
 
-  <script>
-   document.getElementById("switch-to-signup").addEventListener("click", function(event) {
-  event.preventDefault();
-
-  document.getElementById("login-form").classList.add("fade-out");
-  document.getElementById("signup-form").classList.remove("fade-out");
-
-  setTimeout(function() {
-    document.getElementById("login-form").style.display = "none";
-    document.getElementById("signup-form").style.display = "block";
-    document.getElementById("login-form").classList.remove("fade-out");
-    document.getElementById("signup-form").classList.add("fade-in");
-  }, 500);
-});
-
-document.getElementById("switch-to-login").addEventListener("click", function(event) {
-  event.preventDefault();
-
-  document.getElementById("signup-form").classList.add("fade-out");
-  document.getElementById("login-form").classList.remove("fade-out");
-
-  setTimeout(function() {
-    document.getElementById("signup-form").style.display = "none";
-    document.getElementById("login-form").style.display = "block";
-    document.getElementById("signup-form").classList.remove("fade-out");
-    document.getElementById("login-form").classList.add("fade-in");
-  }, 500);
-});
-
-  </script>
+<script src="../js/login.js"></script>
 </body>
 </html>
